c07df848c0e4960e44fa074ef86e6876f5f14aad
Brian Farnier <brian.pro1993@gmail.com>
2025-09-18 22:01:05 +0200

    update tableau add mini-programe

--- Tableau/compteurMot.php
+++ Tableau/compteurMot.php
@@ -5,5 +5,24 @@
 // • Ignorez la casse (majuscule/minuscule). 
 // • Supprimez la ponctuation pour un vrai comptage. 
 
+$phrase = "Et sans Pierre il y pas de pierre mais avec les pierre on fait des pyramide !";
 
+$phrase = strtolower($phrase);
+$phrase = str_replace(["!","?","&"], "", $phrase);
+
+$mots = explode(" ", $phrase );
+
+$compteur  = [];
+
+
+ foreach($mots as $stockMot){
+
+    if(isset($compteur[$stockMot])){
+        $compteur[$stockMot]++;
+    }else{
+        $compteur[$stockMot] = 1 ; 
+ }
+ }
+
+ print_r($compteur);
 ?>
---
+++ mini-programe/GestionStock.php
@@ -0,0 +1,11 @@
+<?php
+// . Gestion de Stock 
+// Créez un programme qui gère un inventaire sous forme de tableau associatif 
+// (article => quantité). Le programme doit permettre : 
+// • Ajouter un article avec une quantité. 
+// • Mettre à jour la quantité d’un article. 
+// • Supprimer un article. 
+// • Afficher les articles dont le stock est inférieur à 10. 
+
+
+?>
